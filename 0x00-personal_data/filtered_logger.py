e845a6dc6c0d62a03b56d601f2684d7213f170fb
AdwoaSK <106457557+AdwoaSK@users.noreply.github.com>
2024-12-24 18:11:45 +0000

    Add task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module to filter and obfuscate Personally Identifiable Information (PII)
+in log messages.
+"""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Filters out specified fields in a log message and replaces them with
+    the redaction string.
+
+    Args:
+        fields (list): List of fields to obfuscate.
+        redaction (str): String used to obfuscate the fields.
+        message (str): Log message containing the data to be obfuscated.
+        separator (str): Character used to separate fields in the message.
+
+    Returns:
+        str: The obfuscated log message.
+    """
+    return re.sub(
+        r'({}=[^{}]+)'.format('|'.join(fields), separator),
+        lambda x: f"{x.group(0).split('=')[0]}={redaction}",
+        message
+    )
